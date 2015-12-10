b9867bdc43008e418491cb2cef7b241d9356369b
dani-lo <resina00@gmail.com>
2015-12-10 19:24:45 +0000

    adding files for js logic (stubs)

--- dist/views/app.js
+++ dist/views/app.js
@@ -1,1 +1,1 @@
-define(["backbone"],function(a){var b=a.View.extend({initialize:function(){console.log("Wahoo!")}});return b});
+define(["backbone","views/pages/slots"],function(a,b){var c=a.View.extend({el:".timeslotter",initialize:function(){console.log("Wahoo!");var a=new b({el:".slots-view"});a.initialize().render()}});return c});
--- scripts/views/app.js
+++ scripts/views/app.js
@@ -1,7 +1,15 @@
-define(['backbone'], function(Backbone) {
+define([
+	'backbone',
+	'views/pages/slots'
+], function(Backbone, SlotsPage) {
   var App = Backbone.View.extend({
+  	el: ".timeslotter",
     initialize: function() {
       console.log( 'Wahoo!' );
+      var slotsPage = new SlotsPage({
+      	el: ".slots-view"
+      });
+      slotsPage.initialize().render();
     }
   });
 
